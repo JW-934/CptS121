00e5ad1f42a48261efbdfe95604f8146af81db36
Jesse <jessew7980@gmail.com>
2021-09-01 15:46:39 -0700

    Update

--- Lab1Task2/Lab1Task2/main.c
+++ Lab1Task2/Lab1Task2/main.c
@@ -9,34 +9,23 @@
 #define _CRT_SECURE_NO_WARNINGS // necessary to ignore scanf_s () warnings
 #define PI 3.141592653
 
-
 #include <stdio.h> // necessary to use printf () and scanf ()
 
-
-
 int main(void) // the starting point for all C programs
-
 {
-
     // we need to request memory for
 
     int number1_int = 0, number2_int = 0; // 2 variable declarations - reserves two memory blocks for integers and sets them to 0's
 
     double number1_float = 0.0, number2_float = 0.0; // reserves two memory blocks for numbers with high precision (floating-point)
 
-
-
     printf("Enter two integer values: "); // ask the user
 
     scanf("%d%d", &number1_int, &number2_int); // read the integers typed through the keyboard - %d is for "decimal", i.e. integers
 
-
-
     printf("Enter two floating-point values: "); // ask the user
 
     scanf("%lf%lf", &number1_float, &number2_float); // read the integers typed through the keyboard - %lf is for "long float", i.e. double precision numbers
-
-
 
     // place all code for the subtasks/operations below here
 
@@ -64,7 +53,6 @@ int main(void) // the starting point for all C programs
     //Determine if number1_int and number2_int are even or odd. Print 0 if even and 1 if odd. You may not use “if” statements. Use the mod operator instead.
     printf("Int 1 Odd: %d Int 2 Odd: %d\n", number1_int % 2, number2_int % 2);
 
-
     //b. Prompts the user for inputs into Ohm’s Law and determines the voltage. Print the voltage value.
     int current = 0, resistance = 0;
     
@@ -87,7 +75,6 @@ int main(void) // the starting point for all C programs
     scanf("%d %d %d %d %d", &a, &b, &c, &d, &x);
     printf("y = %lf", (double)(3 * (a * (x * x * x)) + ((double)1 / 4) * (b * (x * x)) + (10 * (c * x)) + (-5 * d)));
  
-
     //e. Prompts the user for inputs into the circumference of circle equation provided and determines the circumference value
     double radius = 0;
     printf("Enter a circle's radius: ");
