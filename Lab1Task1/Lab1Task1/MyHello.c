253080861aa7323ce6cc27268fe52557392116e4
Jesse <jessew7980@gmail.com>
2021-09-01 14:13:29 -0700

    Lab1Task1 created

---
+++ Lab1Task1/Lab1Task1/MyHello.c
@@ -0,0 +1,17 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section
+* Programming Assignment: Lab1Task1
+* Date: September 1, 2021
+* Description: This program prints out a simple "hello world" message.
+*/
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("Hello World, I am in CptS 121!\n");
+
+	return 0;
+}
