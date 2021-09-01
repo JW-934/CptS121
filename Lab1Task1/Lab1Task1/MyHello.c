886f081b84eff5c42b0a8c8c66007f7b5967acd1
Jesse <jessew7980@gmail.com>
2021-09-01 14:19:24 -0700

    Update

--- Lab1Task1/Lab1Task1/MyHello.c
+++ Lab1Task1/Lab1Task1/MyHello.c
@@ -1,13 +1,12 @@
 /*
 * Programmer: Jesse Watson
-* Class: CptS 121, Fall 2021; Lab Section
+* Class: CptS 121, Fall 2021; Lab Section 7
 * Programming Assignment: Lab1Task1
 * Date: September 1, 2021
 * Description: This program prints out a simple "hello world" message.
 */
 
 #include <stdio.h>
-
 
 int main(void)
 {
