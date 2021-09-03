24c9f99a30b92315ff3bd8ae6703ba6646dcd17c
Jesse <jessew7980@gmail.com>
2021-09-03 11:43:05 -0700

    GPA created

---
+++ GPA/GPA/main.c
@@ -0,0 +1,26 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Date: September 3, 2021
+* Description: Prompts for credits possible in 3 classes, corresponding grade points,
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int credits1 = 0, credits2 = 0, credits3 = 0;
+	double gp1 = 0, gp2 = 0, gp3 = 0;
+
+	
+
+
+
+
+
+
+	return 0;
+}
