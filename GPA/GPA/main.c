87b6ab42becc775430f1d7819cf14fbc515e4d91
Jesse <jessew7980@gmail.com>
2021-09-08 11:55:02 -0700

    Update

--- GPA/GPA/main.c
+++ GPA/GPA/main.c
@@ -10,6 +10,12 @@
 #include <math.h>
 #include <stdio.h>
 
+// function declaration - function prototype
+// double f(double x);
+double get_gp(int class_num);
+
+
+
 int main(void)
 {
 	int credits1 = 0, credits2 = 0, credits3 = 0,
@@ -17,20 +23,26 @@ int main(void)
 	double gp1 = 0, gp2 = 0, gp3 = 0, weighted_gp = 0,
 		gpa = 0;
 
-	printf("Enter grade point for class 1: ");
-	scanf("%lf", &gp1);
+	/*printf("Enter grade point for class 1: ");
+	scanf("%lf", &gp1);*/
+
+	gp1 = get_gp(1);
 
 	printf("Enter credits for class 1: ");
 	scanf("%d", &credits1);
 	
-	printf("Enter grade point for class 2: ");
-	scanf("%lf", &gp2);
+	/*printf("Enter grade point for class 2: ");
+	scanf("%lf", &gp2);*/
 
+	gp2 = get_gp(2);
+	
 	printf("Enter credits for class 2: ");
 	scanf("%d", &credits2);
 	
-	printf("Enter grade point for class 3: ");
-	scanf("%lf", &gp3);
+	/*printf("Enter grade point for class 3: ");
+	scanf("%lf", &gp3);*/
+
+	gp3 = get_gp(3);
 
 	printf("Enter credits for class 3: ");
 	scanf("%d", &credits3);
@@ -45,8 +57,26 @@ int main(void)
 	printf("GPA: %.2lf\n", gpa);
 
 
-
+	//result = f(2);
 
 
 	return 0;
 }
+
+/*double f(double x) // function header
+{
+	return x * x;
+}
+*/
+
+double get_gp(int class_num)
+{
+	double gp = 0;
+
+	printf("Enter grade point for class %d: ", class_num);
+	scanf("%lf", &gp);
+
+
+	return gp;
+}
+
