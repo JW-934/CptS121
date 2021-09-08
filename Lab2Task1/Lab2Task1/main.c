9003e5a4fb462a31383b1071c1041dfbf13b8050
Jesse <jessew7980@gmail.com>
2021-09-08 14:11:46 -0700

    Lab2Task1 created

---
+++ Lab2Task1/Lab2Task1/main.c
@@ -0,0 +1,21 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: Lab1Task1
+* Date: September 8, 2021
+* Description: This program 
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+
+
+}
+
+
+
