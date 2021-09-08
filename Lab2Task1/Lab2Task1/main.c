1dd85832c9648a59e0f2bcfb770a03cb3974d6fc
Jesse <jessew7980@gmail.com>
2021-09-08 14:35:15 -0700

    Task 1 complete

--- Lab2Task1/Lab2Task1/main.c
+++ Lab2Task1/Lab2Task1/main.c
@@ -1,9 +1,11 @@
 /*
 * Programmer: Jesse Watson
 * Class: CptS 121, Fall 2021; Lab Section 7
-* Programming Assignment: Lab1Task1
+* Programming Assignment: Lab2Task1
 * Date: September 8, 2021
-* Description: This program 
+* Description: This program outputs the equation 
+*			   of the perpendicular bisector of the line segment 
+*			   between two points
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -13,8 +15,33 @@
 
 int main(void)
 {
+	double x1 = 0, x2 = 0, y1 = 0, y2 = 0, slope = 0, midx = 0, midy = 0, bisector_slope = 0,
+		bisector_yint = 0;
+	
+	// Prompt for points
+	printf("Enter the coordinates of two points (x, y x2, y2): ");
+	scanf("%lf, %lf %lf, %lf", &x1, &y1, &x2, &y2);
 
+	// Find slope
+	slope = (y2 - y1) / (x2 - x1);
 
+	// Find midpoint
+	midx = (x1 + x2) / 2;
+	midy = (y1 + y2) / 2;
+
+	// Find slope of perpendicular bisector
+	bisector_slope = (double)-1 / slope;
+
+	// Find y intercept of bisector
+	bisector_yint = midy - (bisector_slope * midx);
+	
+	// Display result
+	printf("Equation of perpendicular bisector for points (%.1lf, %.1lf) and (%.1lf, %.1lf): y = %.1lfx + %.1lf", x1, y1, x2, y2, bisector_slope, bisector_yint);
+	
+	
+	
+	
+	return 0;
 }
 
 
