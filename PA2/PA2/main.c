6e33727137b852d473c0c1601f90d79c2552c131
Jesse <jessew7980@gmail.com>
2021-09-08 16:11:32 -0700

    PA2

--- PA2/PA2/main.c
+++ PA2/PA2/main.c
@@ -7,6 +7,7 @@
 */
 
 #define _CRT_SECURE_NO_WARNINGS
+#define PI 3.14159
 
 #include <stdio.h>
 #include <math.h>
@@ -16,6 +17,8 @@ double calculate_total_sales_tax(double sales_tax_rate, double item_cost);
 double calculate_volume_pyramid(double l, double w, double h);
 double calc_para_resist(int r1, int r2, int r3);
 char encode_char(char character, int shift);
+double find_distance(double x1, double y1, double x2, double y2);
+double eval_general_eq(int a, double x, double y, double z);
 
 int main(void)
 {
@@ -56,15 +59,39 @@ int main(void)
 	//parallel_resist = calc_para_resist(newR1, newR2, newR3);
 	//printf("Total parallel resistance: %.2lf", parallel_resist);
 
-	//Character encoding
-	char plaintext = ' ', new_char = ' ';
-	int shift = 0;
+	////Character encoding
+	//char plaintext = ' ', new_char = ' ';
+	//int shift = 0;
 
-	printf("Enter one character and a shift integer (a, shift): ");
-	scanf("%c, %d", &plaintext, &shift);
+	//printf("Enter one character and a shift integer (a, shift): ");
+	//scanf("%c, %d", &plaintext, &shift);
 
-	new_char = encode_char(plaintext, shift);
-	printf("Encoded Character: %c", new_char);
+	//new_char = encode_char(plaintext, shift);
+	//printf("Encoded Character: %c", new_char);
+
+	////Distance between two points
+	//double x1 = 0, x2 = 0, y1 = 0, y2 = 0, distance = 0;
+
+	//printf("Enter two coordinate points (x1, y1 x2, y2):");
+	//scanf("%lf, %lf %lf, %lf", &x1, &y1, &x2, &y2);
+
+	//distance = find_distance(x1, y1, x2, y2);
+	//printf("Distance between points: %.2lf", distance);
+
+	//General Equation
+	int a = 0;
+	double x = 0, y = 0, z = 0, result = 0;
+	printf("General equation: enter a, x, y, z (a is an integer): ");
+	scanf("%d, %lf, %lf, %lf", &a, &x, &y, &z);
+
+	result = eval_general_eq(a, x, y, z);
+	printf("Result: %.2lf", result);
+
+
+
+
+
+
 
 
 
@@ -95,9 +122,15 @@ char encode_char(char character, int shift)
 	return((character - 'a') + 'A' - shift);
 }
 
+double find_distance(double x1, double y1, double x2, double y2)
+{
+	return(sqrt((pow(x1 - x2, 2) + pow(y1 - y2, 2))));
+}
 
-
-
+double eval_general_eq(int a, double x, double y, double z)
+{
+	return((y / (3.0 / 17) - z + (x / (a % 2)) + PI));
+}
 
 
 
