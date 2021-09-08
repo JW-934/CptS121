494f0d02555f495a5eb7fea684d574608b19db61
Jesse <jessew7980@gmail.com>
2021-09-08 15:08:58 -0700

    Update

--- Lab2/Lab2/main.c
+++ Lab2/Lab2/main.c
@@ -3,8 +3,8 @@
 * Class: CptS 121, Fall 2021; Lab Section 7
 * Programming Assignment: Lab2
 * Date: September 8, 2021
-* Description: This program outputs the equation 
-*			   of the perpendicular bisector of the line segment 
+* Description: This program outputs the equation
+*			   of the perpendicular bisector of the line segment
 *			   between two points, calculates BMI, and calculates
 *			   BCS Score.
 */
@@ -19,7 +19,7 @@ int main(void)
 	// Perpendicular bisector calculations
 	double x1 = 0, x2 = 0, y1 = 0, y2 = 0, slope = 0, midx = 0, midy = 0, bisector_slope = 0,
 		bisector_yint = 0;
-	
+
 	// Prompt for points
 	printf("Enter the coordinates of two points (x, y x2, y2): ");
 	scanf("%lf, %lf %lf, %lf", &x1, &y1, &x2, &y2);
@@ -36,17 +36,17 @@ int main(void)
 
 	// Find y intercept of bisector
 	bisector_yint = midy - (bisector_slope * midx);
-	
+
 	// Display result
 	printf("Equation of perpendicular bisector for points (%.1lf, %.1lf) and (%.1lf, %.1lf): y = %.1lfx + %.1lf", x1, y1, x2, y2, bisector_slope, bisector_yint);
-	
+
 	// BMI Calculations
 	double weight = 0, height = 0, height_inches = 0;
-	
+
 	// Prompt for weight and height
 	printf("Enter your weight(lbs) and height(ft) (w h): ");
 	scanf("%lf %lf", &weight, &height);
-	
+
 	// Convert feet to inches
 	height_inches = height * 12;
 
@@ -54,10 +54,10 @@ int main(void)
 	printf("BMI: %lf", (weight / (height_inches * height_inches)) * 703);
 
 	//BCS Score
-	
+
 	double harris_poll = 0, coaches_poll = 0, computer_ranking = 0;
-	
-	
+
+
 	//Prompt for harris poll, coaches poll, and computer ranking
 	printf("Enter harris poll, coaches poll, and computer ranking scores (harris, coaches, computer): ");
 	scanf("%lf, %lf, %lf", &harris_poll, &coaches_poll, &computer_ranking);
