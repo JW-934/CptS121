ea7d398820d803b0c73cb3d78568974f48987f1c
Jesse <jessew7980@gmail.com>
2021-09-08 16:20:00 -0700

    PA2

--- PA2/PA2/main.c
+++ PA2/PA2/main.c
@@ -13,70 +13,70 @@
 #include <math.h>
 
 int calculate_series_resistance(int r1, int r2, int r3);
+char encode_char(char character, int shift);
 double calculate_total_sales_tax(double sales_tax_rate, double item_cost);
 double calculate_volume_pyramid(double l, double w, double h);
 double calc_para_resist(int r1, int r2, int r3);
-char encode_char(char character, int shift);
 double find_distance(double x1, double y1, double x2, double y2);
 double eval_general_eq(int a, double x, double y, double z);
 
 int main(void)
 {
-	////Total series resistance
-	//int R1 = 0, R2 = 0, R3 = 0, series_resistance = 0;
-	//
-	//printf("Enter the values of 3 resistors connected in series (integers): ");
-	//scanf("%d %d %d", &R1, &R2, &R3);
-	//
-	//series_resistance = calculate_series_resistance(R1, R2, R3);
-	//printf("Series Resistance: %d", series_resistance);
+	//Total series resistance
+	int R1 = 0, R2 = 0, R3 = 0, series_resistance = 0;
+	
+	printf("Enter the values of 3 resistors connected in series (integers): ");
+	scanf("%d %d %d", &R1, &R2, &R3);
+	
+	series_resistance = calculate_series_resistance(R1, R2, R3);
+	printf("Series Resistance: %d\n", series_resistance);
 
-	////Total Sales Tax
-	//double item = 0, tax = 0, total_tax = 0;
+	//Total Sales Tax
+	double item = 0, tax = 0, total_tax = 0;
 
-	//printf("Enter item cost and tax rate percentage (cost, tax):");
-	//scanf("%lf, %lf", &item, &tax);
+	printf("Enter item cost and tax rate percentage (cost, tax): ");
+	scanf("%lf, %lf", &item, &tax);
 
-	//total_tax = calculate_total_sales_tax(tax, item);
-	//printf("Total tax: %.2lf", total_tax);
+	total_tax = calculate_total_sales_tax(tax, item);
+	printf("Total tax: %.3lf\n", total_tax);
 
-	////Volume of right rectangular pyramid
-	//double length = 0, width = 0, height = 0, volume = 0;
+	//Volume of right rectangular pyramid
+	double length = 0, width = 0, height = 0, volume = 0;
 
-	//printf("Enter the dimensions of the right rectangular pyramid (l, w, h): ");
-	//scanf("%lf, %lf, %lf", &length, &width, &height);
+	printf("Enter the dimensions of the right rectangular pyramid (l, w, h): ");
+	scanf("%lf, %lf, %lf", &length, &width, &height);
 
-	//volume = calculate_volume_pyramid(length, width, height);
-	//printf("Volume: %.2lf", volume);
+	volume = calculate_volume_pyramid(length, width, height);
+	printf("Volume: %.3lf\n", volume);
 
-	////Total parallel resistance
-	//int newR1 = 0, newR2 = 0, newR3 = 0;
-	//double parallel_resist = 0;
+	//Total parallel resistance
+	int newR1 = 0, newR2 = 0, newR3 = 0;
+	double parallel_resist = 0;
 
-	//printf("Enter the values of 3 resistors connected in parallel (integers): ");
-	//scanf("%d %d %d", &newR1, &newR2, &newR3);
+	printf("Enter the values of 3 resistors connected in parallel (integers): ");
+	scanf("%d %d %d", &newR1, &newR2, &newR3);
 
-	//parallel_resist = calc_para_resist(newR1, newR2, newR3);
-	//printf("Total parallel resistance: %.2lf", parallel_resist);
+	parallel_resist = calc_para_resist(newR1, newR2, newR3);
+	printf("Total parallel resistance: %.3lf\n", parallel_resist);
 
-	////Character encoding
-	//char plaintext = ' ', new_char = ' ';
-	//int shift = 0;
+	//Character encoding
+	char plaintext = ' ', new_char = ' ';
+	int shift = 0;
 
-	//printf("Enter one character and a shift integer (a, shift): ");
-	//scanf("%c, %d", &plaintext, &shift);
+	printf("Enter one character and a shift integer (a, shift): ");
+	scanf(" %c, %d", &plaintext, &shift);
 
-	//new_char = encode_char(plaintext, shift);
-	//printf("Encoded Character: %c", new_char);
+	new_char = encode_char(plaintext, shift);
+	printf("Encoded Character: %c\n", new_char);
 
-	////Distance between two points
-	//double x1 = 0, x2 = 0, y1 = 0, y2 = 0, distance = 0;
+	//Distance between two points
+	double x1 = 0, x2 = 0, y1 = 0, y2 = 0, distance = 0;
 
-	//printf("Enter two coordinate points (x1, y1 x2, y2):");
-	//scanf("%lf, %lf %lf, %lf", &x1, &y1, &x2, &y2);
+	printf("Enter two coordinate points (x1, y1 x2, y2):");
+	scanf("%lf, %lf %lf, %lf", &x1, &y1, &x2, &y2);
 
-	//distance = find_distance(x1, y1, x2, y2);
-	//printf("Distance between points: %.2lf", distance);
+	distance = find_distance(x1, y1, x2, y2);
+	printf("Distance between points: %.3lf\n", distance);
 
 	//General Equation
 	int a = 0;
@@ -85,16 +85,7 @@ int main(void)
 	scanf("%d, %lf, %lf, %lf", &a, &x, &y, &z);
 
 	result = eval_general_eq(a, x, y, z);
-	printf("Result: %.2lf", result);
-
-
-
-
-
-
-
-
-
+	printf("Result: %.3lf\n", result);
 }
 
 int calculate_series_resistance(int r1, int r2, int r3)
@@ -131,11 +122,3 @@ double eval_general_eq(int a, double x, double y, double z)
 {
 	return((y / (3.0 / 17) - z + (x / (a % 2)) + PI));
 }
-
-
-
-
-
-
-
-
