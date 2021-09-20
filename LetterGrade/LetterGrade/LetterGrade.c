c8f49c4b13e6ce41fa089e9b818ef725800a95e0
Jesse <jessew7980@gmail.com>
2021-09-20 11:59:55 -0700

    LetterGrade

--- LetterGrade/LetterGrade/LetterGrade.c
+++ LetterGrade/LetterGrade/LetterGrade.c
@@ -13,15 +13,51 @@ char determine_letter_grade(int score) // function header
 
 	// start of solution 1
 	// Ranges: A - greater than or equal to 90
-	if (score >= 90) // (conditional statement - relational - Boolean)
+	//if (score >= 90) // (conditional statement - relational - Boolean)
+	//{
+	//	// if statement body
+	//	letter_grade = 'A';
+	//}
+	//if ((score < 90) && (score >= 80)) // && logical AND (compound condition)
+	//{
+	//	letter_grade = 'B';
+	//}
+	//if (score >= 70 && score < 80)
+	//{
+	//	letter_grade = 'C';
+	//}
+	//if (score >= 60 && score < 70)
+	//{
+	//	letter_grade = 'D';
+	//}
+	//if (score < 60)
+	//{
+	//	letter_grade = 'F';
+	//}
+	
+	
+	if (score >= 90)
 	{
-		// if statement body
 		letter_grade = 'A';
 	}
-	if ((score < 90) && (score >= 80)) // && logical AND (compound condition)
+	else if (score < 90) // else only activated if prior if is false
 	{
 		letter_grade = 'B';
 	}
+	else if (score >= 70)
+	{
+		letter_grade = 'C';
+	}
+	else if (score >= 60)
+	{
+		letter_grade = 'D';
+	}
+	else // else does not use a condition, this one only runs if all others false
+	{
+		letter_grade = 'F';
+	}
+	
+	
 	// we are not done with this solution yet! we know
 	// we're going to need more if statements for the other
 	// score ranges!
