831e46de296bd2da4fe48c69e30beb42d5971e12
Jesse <jessew7980@gmail.com>
2021-09-29 11:51:29 -0700

    Loops

--- Examples/Loops/Loops/main.c
+++ Examples/Loops/Loops/main.c
@@ -13,16 +13,36 @@
 
 int main(void)
 {
-	int count = 1; // initialization expression - control variable
+	int count = 1, count2 = 0; // initialization expression - control variable
 	int number = 0;
 
 	FILE* infile = fopen("numbers.txt", "r");
 
-	while (!feof (infile))
+	while (!feof (infile)) // file end of file (true when at end of file)
 	{
 		fscanf(infile, "%d", &number);
 		printf("feof file loop: %d\n", number);
 	}
+
+	// nested loops
+	for (count = 0; count < 20; ++count) // outer loop
+	{
+		for (count2 = 0; count2 < 10; ++count2) // inner loop
+		{
+			printf("count: %d, count2: %d\n", count, count2);
+		}
+	}
+
+	//for (infile = fopen ("numbers.txt", "r"); !feof(infile); fscanf(infile, "%d", &number)) //Can leave parts of initialization statement blank
+	//{
+	//	printf("feof file loop: %d\n", number);
+	//}
+
+	//do //assumes that the file isn't empty
+	//{
+	//	fscanf(infile, "%d", &number);
+	//	printf("feof file loop: %d\n", number);
+	//} while (!feof (infile));
 
 	fclose(infile);
 	//printf("1\n");
