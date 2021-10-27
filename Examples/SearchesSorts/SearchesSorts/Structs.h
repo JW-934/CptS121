8cf36c0786b8d5651123db1d085eea672c3257b8
Jesse <jessew7980@gmail.com>
2021-10-27 11:59:13 -0700

    SearchesSorts

--- Examples/SearchesSorts/SearchesSorts/Structs.h
+++ Examples/SearchesSorts/SearchesSorts/Structs.h
@@ -4,8 +4,8 @@
 
 typedef struct rectangle
 {
-	double length;
-	double width;
+	double length; // nice to keep fields on separate lines to see them easier
+	double width;  // cannot set any variables in declaration, no memory allocated yet
 } Rectangle;
 
 //typedef struct rectangle Rectangle;
