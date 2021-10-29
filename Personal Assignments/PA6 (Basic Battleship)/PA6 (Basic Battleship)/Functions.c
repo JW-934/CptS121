fc4bb26340d5b34eb5a1b98d342fe812d52aac3c
Jesse <jessew7980@gmail.com>
2021-10-29 11:23:56 -0700

    PA6

---
+++ Personal Assignments/PA6 (Basic Battleship)/PA6 (Basic Battleship)/Functions.c
@@ -0,0 +1,10 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: PA6
+* Date: October 29, 2021
+* Description: This program is a game of Battleship.
+*/
+
+#include "PA6.h"
+
---
+++ Personal Assignments/PA6 (Basic Battleship)/PA6 (Basic Battleship)/PA6.h
@@ -0,0 +1,17 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: PA6
+* Date: October 29, 2021
+* Description: This program is a game of Battleship.
+*/
+#ifndef PA6_H
+
+#define PA6_H
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#endif
