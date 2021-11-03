b6ea148806cc9718dcea818e94d6f534755d9bb0
Jesse <jessew7980@gmail.com>
2021-11-03 15:56:14 -0700

    Lab10Task2

---
+++ Labs/Lab10/Lab10Task2 (Tic Tac Toe)/Lab10Task2 (Tic Tac Toe)/Lab10Task2.c
@@ -0,0 +1,9 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: Lab10Task2
+* Date: November 3, 2021
+* Description: This program is a game of tic tac toe.
+*/
+
+#include "Lab10Task2.h"
---
+++ Labs/Lab10/Lab10Task2 (Tic Tac Toe)/Lab10Task2 (Tic Tac Toe)/Lab10Task2.h
@@ -0,0 +1,29 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: Lab10Task1
+* Date: October 27, 2021
+* Description: This program generates random sentences.
+*/
+
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: Lab10Task2
+* Date: November 3, 2021
+* Description: This program is a game of tic tac toe.
+*/
+
+#ifndef LAB10TASK2_H
+
+#define LAB10TASK2_H
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#endif
