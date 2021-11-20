8fb33c2d8add63eb1ac6a417e21f1c6ad851c55d
Jesse <jessew7980@gmail.com>
2021-11-20 13:47:41 -0800

    PA7

--- Programming Assignments/PA7 (Poker)/PA7 (Poker)/main.c
+++ Programming Assignments/PA7 (Poker)/PA7 (Poker)/main.c
@@ -2,7 +2,7 @@
 * Programmer: Jesse Watson
 * Class: CptS 121, Fall 2021; Lab Section 7
 * Programming Assignment: PA7
-* Date: November 15, 2021
+* Date: November 15, 2021, November 20, 2021
 * Description: This program is a game of Poker.
 */
 
@@ -10,5 +10,26 @@
 
 int main(void)
 {
+	/* initialize suit array */
+	const char* suit[4] = { "Hearts", "Diamonds", "Clubs", "Spades" };
 
+	/* initialize face array */
+	const char* face[13] = { "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight",
+		"Nine", "Ten", "Jack", "Queen", "King" };
+
+	/* initalize deck array */
+	int deck[4][13] = { 0 };
+
+	srand((unsigned)time(NULL)); /* seed random-number generator */
+
+	shuffle(deck);
+	deal(deck, face, suit);
+
+
+
+
+
+	return 0;
 }
+
+// Example Code Authors: Deitel & Deitel - C How to Program
--- Programming Assignments/PA7 (Poker)/PA7 (Poker)/poker.h
+++ Programming Assignments/PA7 (Poker)/PA7 (Poker)/poker.h
@@ -2,6 +2,37 @@
 * Programmer: Jesse Watson
 * Class: CptS 121, Fall 2021; Lab Section 7
 * Programming Assignment: PA7
-* Date: November 15, 2021
+* Date: November 15, 2021, November 20, 2021
 * Description: This program is a game of Poker.
 */
+
+#ifndef POKER_H
+
+#define POKER_H
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct card
+{
+	int face_ind;
+	int suit_ind;
+}Card;
+
+typedef struct hand
+{
+	Card cards[5];
+}Hand;
+
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char* wFace[], const char* wSuit[]);
+
+
+
+
+
+
+
+#endif
