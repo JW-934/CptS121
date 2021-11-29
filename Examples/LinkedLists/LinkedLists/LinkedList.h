a41ce804c0e3e77e6b96941f8e8060d00b23a71d
Jesse <jessew7980@gmail.com>
2021-11-29 11:33:48 -0800

    LinkedLists

---
+++ Examples/LinkedLists/LinkedLists/LinkedList.h
@@ -0,0 +1,10 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // malloc (), free (), calloc ()
+
+#endif
---
+++ Examples/LinkedLists/LinkedLists/main.c
@@ -0,0 +1,42 @@
+// This example illustrates how to work
+// with dynamic memory and linked lists.
+
+// History: started discussing dynamic memory and malloc ().
+//          we request memory at runtime and try to allocate
+//          only the necessary memory for our program as we need
+//          it/
+
+#include "LinkedList.h"
+
+int main(void)
+{
+	int* mem_ptr = NULL;
+	//int n = 42;
+	int number = 0, count = 0, option = 0;
+
+	printf("Enter the number of integers you want to allocate space for: ");
+	scanf("%d", &number);
+
+	while (count < number)
+	{
+		printf("Enter the value you want to store on heap: ");
+		scanf("%d", &option);
+		// right now we are allocating space on the heap, but
+		// also generating memory "leaks" because we
+		// are only using one pointer!!! we will learn how to 
+		// fix this during lecture on Wednesday, 12/1.
+		mem_ptr = malloc(sizeof(int)); // requests integer number of bytes from heap
+		*mem_ptr = option; // set the indirect value of the pointer
+		                   // to the number entered by the user.
+		printf("Value stored on heap: %d\n", *mem_ptr);
+		++count;
+	}
+
+	mem_ptr = malloc(sizeof(int)); // void * - generic pointer
+	*mem_ptr = 10;
+
+	//mem_ptr = &n;
+
+
+	return 0;
+}
