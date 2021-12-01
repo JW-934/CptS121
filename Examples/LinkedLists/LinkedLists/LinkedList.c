92fa2cb22dcf04aa03632200a061ba44486c782f
Jesse <jessew7980@gmail.com>
2021-12-01 11:49:52 -0800

    LinkedLists

--- Examples/LinkedLists/LinkedLists/LinkedList.c
+++ Examples/LinkedLists/LinkedLists/LinkedList.c
@@ -1,2 +1,11 @@
 #include "LinkedList.h"
 
+Node* make_node(char* item_ptr)
+{
+	Node* mem_ptr = malloc(sizeof(Node));
+
+	mem_ptr->next_ptr = NULL;
+	strcpy(mem_ptr->grocery_item, item_ptr);
+
+	return mem_ptr;
+}
--- Examples/LinkedLists/LinkedLists/LinkedList.h
+++ Examples/LinkedLists/LinkedLists/LinkedList.h
@@ -7,4 +7,14 @@
 #include <string.h>
 #include <stdlib.h> // malloc (), free (), calloc ()
 
+typedef struct node
+{
+	//char* grocery_ites; // Would allocate enough memory for a pointer not a string
+	char grocery_item[25]; // 25 bytes
+	struct node* next_ptr; // 4 bytes // link
+}Node; // Requires 29 bytes
+
+Node* make_node(char* item_ptr);
+	
+
 #endif
