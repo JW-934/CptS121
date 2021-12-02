f1c4a9992b3abea085de872c5a40aef8da19345c
Jesse <jessew7980@gmail.com>
2021-12-01 16:36:14 -0800

    Lab13Task3

---
+++ Labs/Lab13 (Bit Manipulation)/Lab13Task3 (Pack Characters)/Lab13Task3 (Pack Characters)/Main.c
@@ -0,0 +1,9 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 121, Fall 2021; Lab Section 7
+* Programming Assignment: Lab13Task3
+* Date: December 1, 2021
+* Description: This program packs 4 characters into an unsigned int variable.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
